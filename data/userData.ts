68d1b6b58b161a72eaecc8e77459e5e669b0a223
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2025-10-03 11:32:24 -0500

    Auto-generated commit

---
+++ data/userData.ts
@@ -0,0 +1,11 @@
+import { User } from "../models/User";
+
+export const jfsalazar:User = {
+  name: "J F. Salazar Serrano - Tn",
+  alias: "tn",
+  occupation: "Vulnerability researcher",
+  location: "/",
+  email: "twonibblesresearch@gmail.com",
+  ytchannel: "@2Nibbles",
+  imageurl: "https://picsum.photos/id/237/200/300"
+}
